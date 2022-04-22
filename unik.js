3b033615fee85d472097e0f15edc0eca3d0d92a6
viktor7cr7 <volkovatatiana1919@gmail.com>
2022-04-22 20:15:55 +0300

    Answer1

---
+++ unik.js
@@ -0,0 +1,24 @@
+let item_1 = 5
+
+let item_2 = 3
+console.log(item_2)
+
+let item_3 = item_1 + item_2
+console.log(item_3)
+
+let item_4 = "yolochka"
+console.log(item_4)
+console.log(item_3 + item_4)
+console.log(item_3 * item_4)
+
+item_5 = item_3
+
+item_6 = 15
+
+item_6_type = typeof item_6
+console.log("item_6 == " + typeof item_6 , "item_6_type == " + item_6_type)
+
+let item_7 =  String(item_6)
+
+let item_7_type = typeof item_7
+console.log("item_7 == " + typeof item_7, "item_7_type == "+ item_7_type)
